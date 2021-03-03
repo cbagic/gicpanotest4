cf88397aca591374fcfd9a29b67b02a56e850854
cbagic <blankess@nwfsc.edu>
2021-03-03 14:53:45 -0600

    Initial commit

---
+++ data.js
@@ -0,0 +1,223 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano_20210302_101404_0",
+      "name": "PANO_20210302_101404_0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.008676210000004403,
+        "pitch": 0,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.303771218284961,
+          "pitch": -0.03997970246124538,
+          "rotation": 0.7853981633974483,
+          "target": "1-pano_20210302_103001_1"
+        }
+      ],
+      "infoHotspots": [
+          /* array of info hotspots -- edit title and text here, include iframe in text area
+             order of info hotspots determines what's on top - populated in order
+             so later hotspots are on top of earlier ones
+             still learning what yaw and pitch are - probably best to adjust in marzipano
+
+             youtube insertion text:
+             "<iframe width=\"420\" height=\"315\"\n" +
+              "            src=\"https://www.youtube.com/embed/[key]\">\n" +
+              "    </iframe>"
+          */
+        {
+          "yaw": -1.4535697491802928,
+          "pitch": 0.03551790416043765,
+          "title": "waterbottle test edit<br>",
+          "text": " this mystery waterbottle just appeared in the americorps office<br>"
+        },
+        {
+          "yaw": -2.072313411060188,
+          "pitch": 0.40005906987194884,
+          "title": "Renee !!<br>",
+          "text": "Click <u><a href='https://americorps.gov' style='color:#FF5733'>here</a></u> to visit AmeriCorps"
+        },
+        {
+          "yaw": -2.5312967905076817,
+          "pitch": 0.08002541730380308,
+          "title": "Khiara!<br>",
+          "text": "<iframe width=\"420\" height=\"315\"\n" +
+              "            src=\"https://www.youtube.com/embed/XIB5cucDPJ0\">\n" +
+              "    </iframe>"
+        },
+        {
+          "yaw": 3.060894604381329,
+          "pitch": 0.032231158484753664,
+          "title": "Holly's evil twin<br>",
+          "text": " <img src=\"https://i.imgur.com/hcTBstR.jpg\" alt=\"fish diagram\" title=\"source: imgur.com\" style=\"width:420px;height:216px;\" /></a> "
+        },
+        {
+          "yaw": 2.1610060396204336,
+          "pitch": 0.006370823329614694,
+          "title": "Holly<br>",
+          "text": "<audio controls>\n" +
+              "  <source src=\"potato%20whistling.mp3\" type=\"audio/mpeg\">\n" +
+              "Your browser does not support the audio element.\n" +
+              "</audio>"
+        },
+        {
+          "yaw": 1.1514500334870412,
+          "pitch": 0.021577361125672212,
+          "title": "Esmeralda <br>astral projection &lt;3<br>",
+          "text": "<iframe width=\"420\" height=\"315\"\n" +
+              "            src=\"https://www.youtube.com/embed/b2kyy9L1EJI\">\n" +
+              "    </iframe>"
+        },
+        {
+          "yaw": 0.7371333934244895,
+          "pitch": 0.09555304919499008,
+          "title": "Marisa<br>",
+          "text": "This is Marisa"
+        },
+        {
+          "yaw": 0.6403599489906906,
+          "pitch": -0.27753971915164044,
+          "title": "Andrew<br>",
+          "text": "This is Andrew"
+        },
+        {
+          "yaw": 0.46610166785084495,
+          "pitch": 0.24725292717073444,
+          "title": "Esmeralda<br>",
+          "text": "Here is <i>Esmeralda</i>, minus her head"
+        }
+      ]
+    },
+    {
+      "id": "1-pano_20210302_103001_1",
+      "name": "PANO_20210302_103001_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.422441363513144,
+          "pitch": 0.14218977891800222,
+          "rotation": 0,
+          "target": "2-pano_20210302_110530_2"
+        },
+        {
+          "yaw": 2.3634547386820035,
+          "pitch": 0.2065840122555329,
+          "rotation": 0,
+          "target": "0-pano_20210302_101404_0"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5,
+          "pitch": .2,
+          "title": "Test <b>info</b> hotspot",
+          "text": "this is a test info hotspot! I hope it works &lt;3"
+        }
+      ]
+    },
+    {
+      "id": "2-pano_20210302_110530_2",
+      "name": "PANO_20210302_110530_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8743391843779698,
+          "pitch": 0.11760067511426264,
+          "rotation": 0,
+          "target": "1-pano_20210302_103001_1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.821461269733753,
+          "pitch": 0.417167527602734,
+          "title": "This is Khiara<br>",
+          "text": "<div>She has a degree in Nutrition and Food Sciences</div>"
+        },
+        {
+          "yaw": -2.81540489154294,
+          "pitch": 0.2518706073364143,
+          "title": "This is Marisa<br>",
+          "text": "She has a degree in Environmental Science<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
